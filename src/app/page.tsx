0f7bab295e6e1311b268aaaf5d6aea96064dac3d
prajjwalsoni123 <prajjwalsoni123@gmail.com>
2024-05-30 22:48:23 +0530

    single pokemon route addition

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,8 @@
 import { trpc } from "@/utils/trpc";
 
 export default function Home() {
-  const hello = trpc.hello.useQuery(['charizard', 'balbasaur']);
+  const hello = trpc.multiplePokemon.useQuery(['charizard', 'balbasaur']);
+  const hello2 = trpc.singlePokemon.useQuery('balbasaur');
 
 
   if (!hello.data) {
--- src/server/routers/_app.tsx
+++ src/server/routers/_app.tsx
@@ -2,7 +2,7 @@ import { z } from 'zod';
 import { procedure, router } from '../trpc';
 
 export const appRouter = router({
-  hello: procedure
+  multiplePokemon: procedure
     .input(
       z.array(z.string())
     )
@@ -14,6 +14,14 @@ export const appRouter = router({
     }});
      return res;
     }),
+  singlePokemon: procedure.input(z.string()).query(async (opts) => {
+    const res = await opts.ctx.prisma.pokemon.findFirst({
+      where: {
+        name: opts.input,
+      }
+    })
+    return res;
+  })
 });
 
 // export type definition of API
